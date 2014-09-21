2dc7169d9a5f2344b76765bd5e513c54c16e3c71
Valentine Radchuk <Valik@Valentines-MacBook-Air.local>
2014-09-21 15:15:13 +0300

    Show inputs for SLE

--- js/interface.js
+++ js/interface.js
@@ -1,8 +1,24 @@
 /**
  * Created by Valik on 9/21/14.
  */
+
+var currentDimension = 0;
+
+function setDimension(_dimension) {
+    currentDimension = _dimension;
+}
+
+function getDimension(){
+    return currentDimension;
+}
+
 function arrangeMatrix(_dimension, _containerName){
     console.log(_dimension);
+
+    setDimension(_dimension);
+
+    // display button
+    $("#calcButtonPlaceholder").show();
 
     // change selected menu
     changeSelectedMenu(_dimension-2);
@@ -13,35 +29,29 @@ function arrangeMatrix(_dimension, _containerName){
     // build new matrix
 
 
-    var row = equationRow(_dimension, 0);
-    row.appendTo(_containerName);
-
-    var newEquationsDiv = $(document.createElement('div'))
-        .attr("id", 'EquationsDiv');
-
-    newEquationsDiv.after().html('<label>Textbox #' + ' : </label>' +
-        '<input type="text" name="textbox' +
-        '" id="textbox' + '" value="" >');
-
-    /*
-    $(_containerName).appendTo().html(newEquationsDiv);
-
-    newEquationsDiv.appendTo(_containerName);
-    */
-        //html('<h1>SLE rank: ' + _dimension + '</h1>');
+    for(var i=0; i<_dimension; i++) {
+        var row = equationRow(_dimension, i);
+        row.appendTo(_containerName);
+    }
 }
 
 function equationRow(_dimension, _rowNum) {
 
     var equationRowDiv = $(document.createElement('div'))
-        .attr('id', 'equationRowDiv' + _rowNum,
-              'class', 'form-inline');
+        .attr({'id': 'equationRowDiv' + _rowNum,
+              'class': 'form-inline'});
 
-    var rowHtml = "("+_rowNum+")&nbsp;";
+    var rowHtml = "("+(_rowNum+1)+")&nbsp;&nbsp;";
 
     for (var i = 0; i<_dimension; i++) {
-        rowHtml += '<label>x11</label><input class="form-control" style="width: 10px;" type="text" id="text' + _rowNum + i +'" />'
+        rowHtml += '<input class="form-control" style="width: 10px;" type="text" id="text_' + _rowNum + '_' + i +'" /><label>x<sub>'+ (_rowNum+1) + (i+1) +'</sub></label>';
+        if (i != _dimension - 1)
+        {
+            rowHtml += '<label>&nbsp;+&nbsp;</label>'
+        }
     }
+
+    rowHtml += '<label>&nbsp;=&nbsp;</label><input class="form-control" style="width: 10px;" type="text" id="text_' + _rowNum + '_result" />';
 
     equationRowDiv.after().html(rowHtml);
 
@@ -59,4 +69,8 @@ function changeSelectedMenu(_activeLinkNum){
     }
 
     menuAnchors[_activeLinkNum].className = 'list-group-item active';
+}
+
+function getInputsAndCalculate(){
+    alert("Current dimension is:" + getDimension());
 }
